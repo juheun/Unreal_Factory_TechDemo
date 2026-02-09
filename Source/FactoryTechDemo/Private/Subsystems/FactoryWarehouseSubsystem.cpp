584226ec95249d2d763ecea214a301a780ea6b44
juheun <juheun1997@naver.com>
2026-02-09 19:53:54 +0900

    FactoryWarehouseSubsystem 추가

---
+++ Source/FactoryTechDemo/Private/Subsystems/FactoryWarehouseSubsystem.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Subsystems/FactoryWarehouseSubsystem.h"
+
+void UFactoryWarehouseSubsystem::StoreItem(UFactoryItemData* ItemData, const int32 Amount)
+{
+	if (!ItemData) return;
+	
+	int32& StoredAmount = WarehouseInventory.FindOrAdd(ItemData);
+	
+	StoredAmount = FMath::Clamp(StoredAmount + Amount, 0, MaxItemAmount);
+}
+
+bool UFactoryWarehouseSubsystem::TryConsumeItem(UFactoryItemData* ItemData, const int32 Amount)
+{
+	if (!ItemData) return false;
+	
+	int32* StoredAmount = WarehouseInventory.Find(ItemData);
+	
+	if (StoredAmount == nullptr || *StoredAmount < Amount)
+	{
+		return false;
+	}
+	*StoredAmount -= Amount;
+	return true;
+}
+
+int32 UFactoryWarehouseSubsystem::GetItemAmount(UFactoryItemData* ItemData) const
+{
+	if (!ItemData) return 0;
+	
+	const int32* StoredAmount = WarehouseInventory.Find(ItemData);
+	if (StoredAmount == nullptr)
+	{
+		return 0;
+	}
+	return *StoredAmount;
+}
---
+++ Source/FactoryTechDemo/Public/Subsystems/FactoryWarehouseSubsystem.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "FactoryWarehouseSubsystem.generated.h"
+
+class UFactoryItemData;
+
+/**
+ * 유저의 창고를 관리하는 서브시스템
+ */
+UCLASS()
+class FACTORYTECHDEMO_API UFactoryWarehouseSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	void StoreItem(UFactoryItemData* ItemData, const int32 Amount);
+	bool TryConsumeItem(UFactoryItemData* ItemData, const int32 Amount);
+	int32 GetItemAmount(UFactoryItemData* ItemData) const;
+	
+private:
+	UPROPERTY()
+	TMap<TObjectPtr<const UFactoryItemData>, int32> WarehouseInventory;
+	int32 MaxItemAmount = 8000;
+};
