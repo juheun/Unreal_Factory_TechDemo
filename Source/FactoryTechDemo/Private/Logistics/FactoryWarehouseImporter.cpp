0832d33972b86a26b9c6603094242a008f4b5990
juheun <juheun1997@naver.com>
2026-03-17 12:51:46 +0900

    FactoryWarehouseImporter 추가
    DA 및 BP 추가
    Level에 창고 입력 포트를 인벤토리에 추가하는 InteractPoint 추가

---
+++ Source/FactoryTechDemo/Private/Logistics/FactoryWarehouseImporter.cpp
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Logistics/FactoryWarehouseImporter.h"
+
+#include "Items/FactoryItemVisual.h"
+#include "Logistics/FactoryInputPortComponent.h"
+#include "Subsystems/FactoryPoolSubsystem.h"
+#include "Subsystems/FactoryWarehouseSubsystem.h"
+
+
+AFactoryWarehouseImporter::AFactoryWarehouseImporter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AFactoryWarehouseImporter::PlanCycle()
+{
+}
+
+void AFactoryWarehouseImporter::ExecuteCycle()
+{
+	if (!LogisticsInputPortArr.IsValidIndex(0) || !LogisticsInputPortArr[0]) return;
+	
+	UFactoryInputPortComponent* InputPort = LogisticsInputPortArr[0];
+	
+	if (InputPort->PendingItem.ItemData == nullptr) return;
+	
+	if (UFactoryWarehouseSubsystem* WarehouseSubsystem = GetWorld()->GetSubsystem<UFactoryWarehouseSubsystem>())
+	{
+		WarehouseSubsystem->AddItem(InputPort->PendingItem.ItemData, 1);
+		
+		if (AFactoryItemVisual* ItemVisual = InputPort->PendingItem.VisualActor.Get())
+		{
+			if (UFactoryPoolSubsystem* PoolSubsystem = GetGameInstance()->GetSubsystem<UFactoryPoolSubsystem>())
+			{
+				PoolSubsystem->ReturnItemToPool(ItemVisual);
+			}
+			else
+			{
+				ItemVisual->Destroy();
+			}
+			
+			InputPort->PendingItem = FFactoryItemInstance();
+		}
+	}
+}
+
+void AFactoryWarehouseImporter::UpdateView()
+{
+}
+
+bool AFactoryWarehouseImporter::CanPushItemFromBeforeObject(const UFactoryInputPortComponent* RequestPort) const
+{
+	if (!RequestPort) return false;
+	
+	return RequestPort->PendingItem.ItemData == nullptr;
+}
---
+++ Source/FactoryTechDemo/Public/Logistics/FactoryWarehouseImporter.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FactoryLogisticsObjectBase.h"
+#include "FactoryWarehouseImporter.generated.h"
+
+UCLASS()
+class FACTORYTECHDEMO_API AFactoryWarehouseImporter : public AFactoryLogisticsObjectBase
+{
+	GENERATED_BODY()
+
+public:
+	AFactoryWarehouseImporter();
+	
+	virtual void PlanCycle() override;
+	virtual void ExecuteCycle() override;
+	virtual void UpdateView() override;
+	
+	virtual bool CanPushItemFromBeforeObject(const UFactoryInputPortComponent* RequestPort) const override;
+};
