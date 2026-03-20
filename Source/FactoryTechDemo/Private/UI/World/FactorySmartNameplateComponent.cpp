a7280c52ed447e9174f01df4eb7eb3d0c12b0960
juheun <juheun1997@naver.com>
2026-03-20 16:20:32 +0900

    설비 이름표 컴포넌트 (FactorySmartNameplateComponent) 작성

---
+++ Source/FactoryTechDemo/Private/UI/World/FactorySmartNameplateComponent.cpp
@@ -0,0 +1,150 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/World/FactorySmartNameplateComponent.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "Placement/FactoryObjectData.h"
+#include "Placement/FactoryPlaceObjectBase.h"
+#include "Settings/FactoryDeveloperSettings.h"
+
+
+UFactorySmartNameplateComponent::UFactorySmartNameplateComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bStartWithTickEnabled = false;
+	
+	SetHiddenInGame(true);
+	SetWidgetSpace(EWidgetSpace::World);
+	SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void UFactorySmartNameplateComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	float GridLength = 100.f;
+	if (const UFactoryDeveloperSettings* Settings = GetDefault<UFactoryDeveloperSettings>())
+	{
+		GridLength = Settings->GetGridLength();
+	}
+	if (AFactoryPlaceObjectBase* PlaceObject = Cast<AFactoryPlaceObjectBase>(GetOwner()))
+	{
+		if (const UFactoryObjectData* ObjectData = PlaceObject->GetObjectData())
+		{
+			ExtentX = ObjectData->GridSize.X * GridLength * 0.5f;
+			ExtentY = ObjectData->GridSize.Y * GridLength * 0.5f;
+		}
+	}
+	
+	// 뷰모드에 따른 델리게이트 구독
+	if (AFactoryPlayerController* Controller = Cast<AFactoryPlayerController>(
+		UGameplayStatics::GetPlayerController(GetWorld(), 0)))
+	{
+		CachedCurrentViewMode = Controller->GetCurrentViewMode();
+		Controller->OnViewModeChanged.AddDynamic(this, &UFactorySmartNameplateComponent::OnViewModeChanged);
+		
+		OnViewModeChanged(CachedCurrentViewMode);
+	}
+}
+
+void UFactorySmartNameplateComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                                    FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	APlayerCameraManager* CameraManager = UGameplayStatics::GetPlayerCameraManager(GetWorld(), 0);
+	if (!CameraManager) return;
+	
+	FVector CameraLoc = CameraManager->GetCameraLocation();
+	FVector CameraForward = CameraManager->GetCameraRotation().Vector();
+	
+	AActor* OwnerActor = GetOwner();
+	if (!OwnerActor) return;
+
+	FVector OwnerLoc = OwnerActor->GetActorLocation();
+	FVector DirToMachine = (OwnerLoc - CameraLoc).GetSafeNormal();
+	
+	// 카메라에서 벗어난 객체에 대해서는 이름표 표시하지 않음
+	if (FVector::DotProduct(CameraForward, DirToMachine) < CullingDotThreshold)
+	{
+		if (!bHiddenInGame) SetHiddenInGame(true);
+		return;
+	}
+	
+	if (bHiddenInGame) SetHiddenInGame(false);
+	
+	FVector TargetLoc = OwnerLoc;
+	TargetLoc.Z = FixedZHeight;
+	
+	if (CachedCurrentViewMode == EFactoryViewModeType::TopView)
+	{
+		SetWorldLocation(TargetLoc);
+	}
+	else
+	{
+		// 설비 4면 중 카메라와 가장 가까운 면 찾기
+		FVector OwnerForward = OwnerActor->GetActorForwardVector();
+		FVector OwnerRight = OwnerActor->GetActorRightVector();
+		
+		FVector Faces[4] = { OwnerForward, -OwnerForward, OwnerRight, -OwnerRight };
+		FVector DirToCamera2D = (CameraLoc - OwnerLoc).GetSafeNormal2D();
+		
+		int32 BestFaceIndex = 0;
+		float MaxDot = -1.f;
+		
+		for (int32 i = 0; i < 4; i++)
+		{
+			float Dot = FVector::DotProduct(Faces[i], DirToCamera2D);
+			if (Dot > MaxDot)
+			{
+				MaxDot = Dot;
+				BestFaceIndex = i;
+			}
+		}
+		
+		// 목표 위치 설정
+		float PushDistance = (BestFaceIndex == 0 || BestFaceIndex == 1) ? ExtentX : ExtentY;
+		TargetLoc += Faces[BestFaceIndex] * PushDistance;
+
+		// 해당 면에서 바깥쪽을 바라보도록 회전값 설정
+		FRotator TargetRot = Faces[BestFaceIndex].Rotation();
+
+		// 이동/회전 적용
+		FVector NewLoc = FMath::VInterpTo(GetComponentLocation(), TargetLoc, DeltaTime, InterpolationSpeed);
+		FRotator NewRot = FMath::RInterpTo(GetComponentRotation(), TargetRot, DeltaTime, InterpolationSpeed);
+
+		SetWorldLocationAndRotation(NewLoc, NewRot);
+	}
+}
+
+void UFactorySmartNameplateComponent::WakeUp()
+{
+	if (bIsAwake) return;
+	bIsAwake = true;
+	// Tick 내부 계산에 따라 SetHiddenInGame 설정
+	SetComponentTickEnabled(true);
+}
+
+void UFactorySmartNameplateComponent::GoToSleep()
+{
+	if (!bIsAwake) return;
+	bIsAwake = false;
+	SetHiddenInGame(true);
+	SetComponentTickEnabled(false);
+}
+
+void UFactorySmartNameplateComponent::OnViewModeChanged(EFactoryViewModeType NewViewMode)
+{
+	CachedCurrentViewMode = NewViewMode;
+	
+	if (CachedCurrentViewMode == EFactoryViewModeType::TopView)
+	{
+		SetWidgetSpace(EWidgetSpace::Screen);
+	}
+	else
+	{
+		SetWidgetSpace(EWidgetSpace::World);
+	}
+}
+
---
+++ Source/FactoryTechDemo/Public/UI/World/FactorySmartNameplateComponent.h
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "Player/FactoryPlayerController.h"
+#include "FactorySmartNameplateComponent.generated.h"
+
+
+enum class EFactoryViewModeType : uint8;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class FACTORYTECHDEMO_API UFactorySmartNameplateComponent : public UWidgetComponent
+{
+	GENERATED_BODY()
+
+public:
+	UFactorySmartNameplateComponent();
+
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+	
+	UFUNCTION(BlueprintCallable, Category="Factory|Nameplate")
+	void WakeUp();
+	
+	UFUNCTION(BlueprintCallable, Category="Factory|Nameplate")
+	void GoToSleep();
+	
+protected:
+	UFUNCTION()
+	void OnViewModeChanged(EFactoryViewModeType NewViewMode);
+	
+	UPROPERTY(EditDefaultsOnly, Category="Factory|Nameplate")
+	float FixedZHeight = 150.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Factory|Nameplate")
+	float InterpolationSpeed = 10.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Factory|Nameplate")
+	float CullingDotThreshold = 10.f;
+	
+private:
+	EFactoryViewModeType CachedCurrentViewMode = EFactoryViewModeType::NormalView;
+	
+	bool bIsAwake = false;
+	
+	// 설비 그리드 계산 후 캐싱해놓을 오프셋 거리
+	float ExtentX = 50.f;
+	float ExtentY = 50.f;
+};
