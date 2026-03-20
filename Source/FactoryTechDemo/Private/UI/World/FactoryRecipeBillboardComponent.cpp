9bdc4256cdbf860693671fc7ab7c2b2496cce8b7
juheun <juheun1997@naver.com>
2026-03-20 18:09:08 +0900

    레시피 결과물 표시위한 FactoryRecipeBillboardComponent 생성

---
+++ Source/FactoryTechDemo/Private/UI/World/FactoryRecipeBillboardComponent.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/World/FactoryRecipeBillboardComponent.h"
+
+
+UFactoryRecipeBillboardComponent::UFactoryRecipeBillboardComponent()
+{
+
+}
+
+
+void UFactoryRecipeBillboardComponent::UpdateUIPlacement(float DeltaTime, const FVector& CameraLoc, const FVector& CameraForward, const FVector& OwnerLoc)
+{
+	FVector TargetLoc = OwnerLoc;
+	TargetLoc.Z += BillboardZHeight;
+	
+	FRotator CameraRot = CameraForward.Rotation();
+	FRotator TargetRot = FRotator(0.f, CameraRot.Yaw + 180.f, 0.f); // 카메라를 쳐다보도록 180도 뒤집음 (위젯 기준 정면)
+
+	// 부드러운 이동 및 회전
+	FVector NewLoc = FMath::VInterpTo(GetComponentLocation(), TargetLoc, DeltaTime, InterpolationSpeed);
+	FRotator NewRot = FMath::RInterpTo(GetComponentRotation(), TargetRot, DeltaTime, InterpolationSpeed);
+
+	SetWorldLocationAndRotation(NewLoc, NewRot);
+}
---
+++ Source/FactoryTechDemo/Public/UI/World/FactoryRecipeBillboardComponent.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FactoryFacilityWorldUIComponent.h"
+#include "FactoryRecipeBillboardComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class FACTORYTECHDEMO_API UFactoryRecipeBillboardComponent : public UFactoryFacilityWorldUIComponent
+{
+	GENERATED_BODY()
+	
+public:
+	UFactoryRecipeBillboardComponent();
+
+protected:
+	virtual void UpdateUIPlacement(float DeltaTime, const FVector& CameraLoc, const FVector& CameraForward, const FVector& OwnerLoc) override;
+
+	UPROPERTY(EditDefaultsOnly, Category="Factory|Billboard")
+	float BillboardZHeight = 250.f; // 아이콘은 2.5m 높이에 위치
+};
